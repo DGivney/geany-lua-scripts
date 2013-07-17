13b23817de3addd73dfcbbc2ed931e7b4ff1501a
Thrawn <shell_layer-github@yahoo.com.au>
2013-07-17 20:49:09 +1000

    initial commit

---
+++ split-line.lua
@@ -0,0 +1,9 @@
+#! /usr/bin/env lua
+-- Split a line using 'smart indent':
+-- preserve the number of tabs from the previous line,
+-- but use spaces to align to the most recent bracket.
+--
+-- v0.1
+-- (c) 2013 by Carl Antuar.
+-- Distribution is permitted under the terms of the GPLv3
+-- or any later version.
