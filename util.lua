09a286eb9e90b4e20aebe69d71192a02cac1d028
OSIDT <ssqosidt@gmail.com>
2013-10-30 08:22:44 +1000

    enhance utility script with extra useful functions

--- util.lua
+++ util.lua
@@ -9,3 +9,25 @@
 function debugMessage(message)
 	if debugEnabled then geany.message("DEBUG", message) end
 end
+
+function isProjectOpen()
+	return not (geany.appinfo().project == nil)
+end
+
+function getOutputLines(command)
+	local lines = {}
+	local lineCount = 0
+	local result = io.popen(command, 'r')
+	if result == nil then
+		geany.message("ERROR", "Failed to get output of command ["..command.."]")
+		return
+	end
+	for line in result:lines() do
+		-- need to index from 1 to show up properly in choose dialog
+		lineCount = lineCount + 1
+		lines[lineCount] = line
+	end
+	result:close()
+	debugMessage("Returning "..lineCount.." output lines")
+	return lineCount,lines
+end
